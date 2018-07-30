c58eceb5b4bc0e8a20f5585fdb0164cc698d506a
ZackMaya <ingsoftnetozack@outlook.com>
2018-07-30 18:40:33 -0500

    first commit

---
+++ src/uacm/edu/mx/xml2/Xpointer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uacm.edu.mx.xml2;
+
+/**
+ * @author Zack Maya
+ *
+ */
+public class Xpointer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
